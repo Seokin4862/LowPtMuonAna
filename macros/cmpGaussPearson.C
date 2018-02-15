a080ce5e5df5517b33902d494a084f96171aac55
Seokin4862 <sy34@rice.edu>
2018-02-15 15:12:49 -0600

    cmpGaussPearson testdrive fix2

--- macros/cmpGaussPearson.C
+++ macros/cmpGaussPearson.C
@@ -10,6 +10,7 @@ TH1 *h = (TH1*)f->Get( "mu_lambda_vs_p" );
 TH1 *h2 = (TH1*)f2->Get( "mu_lambda_vs_p" );
 
 h->Draw(  );
+h2->SetLineColor( kRed );
 h2->Draw( "same" );
 c->Print( "mu_lambda_vs_p_superimposed.png" );
 
