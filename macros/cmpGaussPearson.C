55bcd97e04bbb6133aff9700fe0659c6873b289c
Seokin4862 <sy34@rice.edu>
2018-02-15 12:55:30 -0600

    cmpGaussPearson testdrive

--- macros/cmpGaussPearson.C
+++ macros/cmpGaussPearson.C
@@ -1,6 +1,16 @@
 void cmpGaussPearson() {
 
+TFile * f = new TFile( "" );
+TFile * f2 = new TFile( "" );
 
 
+TCanvas * c = new TCanvas("c","c");
+
+TH1 *h = (TH1*)f->Get( "mu_lambda_vs_p" );
+TH1 *h2 = (TH1*)f2->Get( "mu_lambda_vs_p" );
+
+h->Draw(  );
+h2->Draw( "same" );
+c->Print( "mu_lambda_vs_p_superimposed" );
 
 }
