204497736cecf4f19146503f54aa099f9d48d902
Seokin4862 <sy34@rice.edu>
2018-02-15 14:13:20 -0600

    cmpGaussPearson testdrive fix1

--- macros/cmpGaussPearson.C
+++ macros/cmpGaussPearson.C
@@ -1,7 +1,7 @@
 void cmpGaussPearson() {
 
-TFile * f = new TFile( "" );
-TFile * f2 = new TFile( "" );
+TFile * f = new TFile( "/home/sy34/workspace/LowPtMuonAna/bin/out/fits_gauss.root" );
+TFile * f2 = new TFile( "/home/sy34/workspace/LowPtMuonAna/bin/out/fits_pearson.root" );
 
 
 TCanvas * c = new TCanvas("c","c");
@@ -11,6 +11,6 @@ TH1 *h2 = (TH1*)f2->Get( "mu_lambda_vs_p" );
 
 h->Draw(  );
 h2->Draw( "same" );
-c->Print( "mu_lambda_vs_p_superimposed" );
+c->Print( "mu_lambda_vs_p_superimposed.png" );
 
 }
