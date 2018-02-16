626f78d0b7d654b10428a475b801809277a8a930
Seokin4862 <sy34@rice.edu>
2018-02-16 03:15:56 -0600

    cmp

--- macros/cmpGaussPearson.C
+++ macros/cmpGaussPearson.C
@@ -14,4 +14,50 @@ h2->SetLineColor( kRed );
 h2->Draw( "same" );
 c->Print( "mu_lambda_vs_p_superimposed.png" );
 
+
+TH1 *h = (TH1*)f->Get( "mu_sigma_vs_p" );
+TH1 *h2 = (TH1*)f2->Get( "mu_sigma_vs_p" );
+
+h->Draw(  );
+h2->SetLineColor( kRed );
+h2->Draw( "same" );
+c->Print( "mu_sigma_vs_p_superimposed.png" );
+
+
+TH1 *h = (TH1*)f->Get( "mu_yield_vs_p" );
+TH1 *h2 = (TH1*)f2->Get( "mu_yield_vs_p" );
+
+h->Draw(  );
+h2->SetLineColor( kRed );
+h2->Draw( "same" );
+c->Print( "mu_yield_vs_p_superimposed.png" );
+
+//pions
+
+TH1 *h = (TH1*)f->Get( "pi_lambda_vs_p" );
+TH1 *h2 = (TH1*)f2->Get( "pi_lambda_vs_p" );
+
+h->Draw(  );
+h2->SetLineColor( kRed );
+h2->Draw( "same" );
+c->Print( "pi_lambda_vs_p_superimposed.png" );
+
+
+TH1 *h = (TH1*)f->Get( "pi_sigma_vs_p" );
+TH1 *h2 = (TH1*)f2->Get( "pi_sigma_vs_p" );
+
+h->Draw(  );
+h2->SetLineColor( kRed );
+h2->Draw( "same" );
+c->Print( "pi_lambda_vs_p_superimposed.png" );
+
+
+TH1 *h = (TH1*)f->Get( "pi_yield_vs_p" );
+TH1 *h2 = (TH1*)f2->Get( "pi_yield_vs_p" );
+
+h->Draw(  );
+h2->SetLineColor( kRed );
+h2->Draw( "same" );
+c->Print( "pi_lambda_vs_p_superimposed.png" );
+
 }
